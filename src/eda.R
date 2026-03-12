7add97f9196b55ec150d782810624e7862d21ccb
Dave Moyer <moyer.david@gmail.com>
2026-03-12 15:55:01 -0700

    adjust claude tests code and finish up ETL

---
+++ src/eda.R
@@ -0,0 +1,11 @@
+## Rieke Plots
+## February 2026
+## Dave Moyer
+
+library(tidyverse)
+library(readxl)
+library(stringr)
+library(sf)
+library(googlesheets4)
+library(janitor)
+library(here)
--- src/public-data-etl.R
+++ src/public-data-etl.R
@@ -1,4 +1,4 @@
-## Rieke Analysis
+## Rieke ETL
 ## February 2026
 ## Dave Moyer
 
@@ -7,9 +7,59 @@ library(readxl)
 library(stringr)
 library(sf)
 library(googlesheets4)
+library(ggmap)
 library(janitor)
 library(here)
 
+# notes ####
+# most files: https://www.oregon.gov/ode/schools-and-districts/reportcards/reportcards/Pages/Accountability-Measures.aspx
+
+# directory ####
+#https://www.ode.state.or.us/instID/
+
+directory_raw <- read_csv('raw/directory/Institution_Search_Results.csv')
+
+directory <- directory_raw %>%
+  filter(`Inst Type` == 'Oregon Public School' & Class == 'School') %>%
+  select(district_id = `Parent ID`,
+         district_name = `Parent Inst`,
+         school_id = Institution_ID,
+         school_name = Directory_Name,
+         address = Street_StrAddr1,
+         city = Street_City,
+         state = Street_State,
+         zip = Street_Zip,
+         county = County) %>%
+  mutate(full_address = paste0(address," ",city,", ",state," ",zip))
+
+gmap_key <- readLines('C:/Users/moyer/work/creds/gmap-key.txt')
+
+register_google(gmap_key)
+
+directory_mapped <- directory %>%
+  mutate_geocode(full_address) 
+
+directory_final <- directory_mapped %>%
+  mutate(
+    lon = case_when(
+      full_address == '6th & Esther Ave Imbler, OR 97841' ~ -117.96489362513624, 
+      full_address == '1700 California Klamath Falls, OR 97601' ~ -121.79583425902996,
+      full_address == '10th & Ingersoll Coos Bay, OR 97420' ~ -124.221804,
+      full_address == 'Fifth & Park Sts Riddle, OR 97469' ~ -123.3620615905213,
+      full_address == '737 Succor Creek Rd Jordan Valley, OR 97910' ~ -117.10827545365018,
+      T ~ lon
+    ),
+    lat  = case_when(
+      full_address == '6th & Esther Ave Imbler, OR 97841' ~ 45.46188021205186,
+      full_address == '1700 California Klamath Falls, OR 97601' ~ 42.23482039511676, 
+      full_address == '10th & Ingersoll Coos Bay, OR 97420' ~ 43.360233, 
+      full_address == 'Fifth & Park Sts Riddle, OR 97469' ~ 42.954195199399734, 
+      full_address == '737 Succor Creek Rd Jordan Valley, OR 97910' ~ 43.31663036407091, 
+      T ~ lat
+    )
+  )
+
+write_csv(directory_final, 'prc/directory.csv', na = "")
 
 # enroll ####
 
@@ -68,7 +118,7 @@ enroll_temp <- enroll_combined %>%
   mutate(
     value_reported = value,
     value = as.numeric(value),
-    value_reported = ifelse(is.na(value),value_reported,NA_character_),
+    value_reported = ifelse(is.na(value), value_reported, NA_character_),
     season = case_when(
       str_detect(source_file,'fall') ~ 'fall',
       str_detect(source_file,'spring') ~ 'spring'
@@ -85,11 +135,11 @@ enroll_temp <- enroll_combined %>%
     str_detect(variable,'2017_18') ~ 2018,
     str_detect(variable,'2018_19') ~ 2019,
     str_detect(variable,'2020_21') ~ 2021,
-    str_detect(variable,'2021_22') ~ 2022,
+    str_detect(variable,'2021_22|2021_2022') ~ 2022,
     str_detect(variable,'2022_23|2022_2023') ~ 2023,
-    str_detect(variable,'2023_24') ~ 2024,
+    str_detect(variable,'2023_24|2023_2024') ~ 2024,
     str_detect(variable,'2024_25|2024_2025') ~ 2025,
-    str_detect(variable,'2025_26') ~ 2026
+    str_detect(variable,'2025_26|2025_2026') ~ 2026
   ),
   level = case_when(
     str_detect(source_sheet,'(?i)school') ~ 'school',
@@ -196,15 +246,32 @@ enroll_temp <- enroll_combined %>%
   ) %>%
   filter((report_year == school_year | is.na(report_year)),
          (str_detect(variable,'ct_') | variable %in% c('all'))) %>%
-  select(-source_file,-source_sheet,-report_year)
+  select(-source_file,-source_sheet,-report_year) %>%
+  mutate(student_group = case_when(
+    str_detect(variable,'all') ~ 'all',
+    T ~ str_extract(variable, '[^_]+$')
+  ),
+  var = case_when(
+    str_detect(variable,'all') ~ 'ct',
+    T~ str_extract(variable, '^[^_]+'))
+  ) %>%
+  select(-variable) %>%
+  distinct() %>%
+  pivot_wider(names_from = c(season,var), values_from = c(value, value_reported)) %>%
+  rename_with(.cols = everything(), ~gsub('value_',"", .x)) %>%
+  mutate(fall_pct = round(100*fall_pct,1)) %>%
+  select(-reported_spring_pct,-reported_fall_pct)
 
-district_enroll <- enroll_temp %>%
-  filter(level == 'district')
+enroll <- enroll_temp
 
-school_enroll <- enroll_temp %>%
-  filter(level == 'school')
+write_csv(enroll, 'prc/enroll.csv', na = '')
 
+# filter groups ####
 
+exclude <- c('non_binary','recent_arrivers','tag','combined_dis',
+             'ever_el','military','migrant','homeless','underserved_race',
+             'extended_assess','incarcerated','foster_care','swd_accom',
+             'indian_ed')
   
 # attend ####
 
@@ -274,14 +341,21 @@ attend_temp <- attend_raw %>%
       "Currently or Formerly Incarcerated"      ~ "incarcerated",
       .default = str_to_lower(str_replace_all(student_group, "[ /]", "_"))
     ),
-    district_id = as.integer(district_id),
-    school_id   = as.integer(school_id),
+    grade = case_when(
+      str_detect(student_group,'grade_') ~ gsub('grade_','',student_group),
+      student_group == 'kindergarten' ~ 'k',
+      T ~ 'all'
+    ),
+    student_group = ifelse(grade != 'all','all',student_group),
     n_regular   = as.numeric(n_regular),
     pct_regular = as.numeric(pct_regular),
     n_absent    = as.numeric(n_absent),
     pct_absent  = as.numeric(pct_absent)
   ) %>%
-  select(-source_file, -any_of(c("report_year", "students_included", "inst_type")))
+  select(-source_file, -any_of(c("report_year", "students_included", "inst_type"))) %>%
+  filter(!(student_group %in% exclude)) %>%
+  relocate(school_year,.before = district_id) %>%
+  relocate(c(grade,student_group), .after = school_name)
 
 district_attend <- attend_temp %>% filter(level == "district")
 school_attend   <- attend_temp %>% filter(level == "school")
@@ -303,11 +377,12 @@ tests_raw <- map(test_files, function(file) {
   bind_rows()
 
 tests_temp <- tests_raw %>%
+  mutate(pct_proficient = coalesce(percent_proficient_level_3_or_4,percent_proficient)) %>%
+  select(-percent_proficient,-percent_proficient_level_3_or_4) %>%
   rename(
     district_name      = any_of("district"),
     school_name        = any_of("school"),
     n_proficient       = any_of("number_proficient"),
-    pct_proficient     = any_of("percent_proficient_level_3_or_4"),
     n_level_4          = any_of("number_level_4"),
     pct_level_4        = any_of("percent_level_4"),
     n_level_3          = any_of("number_level_3"),
@@ -316,7 +391,7 @@ tests_temp <- tests_raw %>%
     pct_level_2        = any_of("percent_level_2"),
     n_level_1          = any_of("number_level_1"),
     pct_level_1        = any_of("percent_level_1"),
-    n_participants     = any_of("number_of_participants")
+    n_tested           = any_of("number_of_participants")
   ) %>%
   mutate(
     school_year = case_when(
@@ -366,9 +441,10 @@ tests_temp <- tests_raw %>%
       "Extended Assessment"                 ~ "extended_assess",
       .default = student_group
     ),
-    grade_level = case_match(
+    grade = case_match(
       grade_level,
       "All Grades" ~ "all",
+      "Grade HS (11)" ~ '11',
       .default = str_remove(grade_level, "^Grade ")
     ),
     across(c(n_proficient, pct_proficient,
@@ -376,14 +452,86 @@ tests_temp <- tests_raw %>%
              n_level_3, pct_level_3,
              n_level_2, pct_level_2,
              n_level_1, pct_level_1,
-             n_participants, participation_rate),
-           as.numeric),
-    district_id = as.integer(district_id),
-    school_id   = as.integer(school_id)
+             n_tested, participation_rate),
+           as.numeric)
   ) %>%
-  select(-source_file, -any_of("academic_year"))
+  select(-source_file, -any_of("academic_year"),-grade_level)
 
-school_tests <- tests_temp
+tests <- tests_temp %>%
+  filter(!(student_group %in% exclude)) %>%
+  filter((grade == 'all' | (grade != 'all' & student_group == 'all'))) %>%
+  relocate(school_year,.before = district_id) %>%
+  relocate(c(subject,grade,student_group), .after = school_name) %>%
+  distinct()
+
+write_csv(tests,'prc/full_tests.csv',na = '')
+
+tests_wide <- tests %>%
+  select(school_year,
+         district_id,
+         district_name,
+         school_id,
+         school_name,
+         grade,
+         student_group,
+         subject,
+         pct_proficient,
+         pct_level_4,
+         pct_level_3,
+         pct_level_2,
+         pct_level_1,
+         n_tested,
+         participation_rate
+         ) %>%
+  pivot_wider(names_from = subject, values_from = c(starts_with('pct'),n_tested), values_fn = {max}) ## a couple places where the participation rate varies
+
+dups <- tests  %>% 
+  mutate(n = dplyr::n(), .by = c(school_year, district_id, district_name, school_id, school_name, grade, student_group,subject)) |>
+  filter(n > 1L) %>%
+  arrange(school_year, district_id, district_name, school_id, school_name, grade, student_group,subject)
 
 
 # funding ####
+funding_files <- list.files("raw/funding",
+                         pattern = "*.xls*",
+                         full.names = TRUE)
+
+funding_raw <- map(funding_files, function(file) {
+  sheets <- excel_sheets(file)
+  data_sheet <- sheets[!str_detect(sheets, "(?i)definition|note")]
+  read_excel(file, sheet = data_sheet[1], col_types = "text") %>%
+    clean_names() %>%
+    mutate(source_file = basename(file))
+}) %>%
+  bind_rows()
+
+funding <- funding_raw %>%
+  mutate(
+    school_year = case_when(
+      str_detect(school_yr, "19-20") ~ 2020L,
+      str_detect(school_yr, "21-22") ~ 2022L,
+      str_detect(school_yr, "22-23") ~ 2023L,
+      str_detect(school_yr, "23-24") ~ 2024L,
+      str_detect(school_yr, "24-25") ~ 2025L
+    )) %>%
+  select(school_year,
+         district_id,
+         district_name,
+         school_id,
+         school_name,
+         total_exp = total_expenditures,
+         per_pupil_exp,
+         fed_per_pupil_exp,
+         state_local_per_pupil_exp) %>%
+  mutate(across(contains("exp"), ~round(as.numeric(.x))))
+
+# combine ####
+analysis <- school_attend %>%
+  select(-level) %>%
+  left_join(tests_wide, by = c('district_id', 'school_id','school_year','grade','student_group'), suffix = c("","_1")) %>%
+  left_join(funding, by = c('district_id','school_id','school_year'), suffix = c("","_2")) %>%
+  select(-school_name_1,-district_name_1,-school_name_2,-district_name_2)
+
+write_csv(analysis, 'prc/attend_tests_funding.csv', na = "")  
+
+    
